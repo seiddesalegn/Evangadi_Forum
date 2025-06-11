e88d7d99ead27810725c4920a808842f4168d97e
seiddesalegn <seiddesalegn4@gmail.com>
2025-06-10 21:50:28 -0700

    answer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,53 @@
-import React, { createContext, useEffect, useState } from 'react'
-import Register from './pages/Register'
-import Home from './pages/Home'
-import Howitworks from './Components/HowItWorks/HowItWorks'
-import { Routes, Route, useNavigate } from 'react-router-dom'
-import UserLogedIn from './Components/Home/UserLogedIn'
-import axiosInstance from './Axios'
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
-import Login from './pages/Login'
-import AskQuestion from './Components/AskQuestion/AskQuestion'
+import React, { createContext, useEffect, useState } from "react";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Howitworks from "./Components/HowItWorks/HowItWorks";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import UserLogedIn from "./Components/Home/UserLogedIn";
+import axiosInstance from "./Axios";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Login from "./pages/Login";
+import AskQuestion from "./Components/AskQuestion/AskQuestion";
+import Answere from "./Components/Answere/Answere";
 
-
-export const AppStates = createContext()
+export const AppStates = createContext();
 function App() {
-  const [user, setUser] = useState([])
-  const token = localStorage.getItem("token")
-  const navigate = useNavigate()
+  const [user, setUser] = useState([]);
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
   async function checkUser() {
-    try{
-      const {data} = await axiosInstance.get("/users/check",{
-        headers : {
-          Authorization: 'Bearer ' + token
-        }
-      })
-      setUser(data)
+    try {
+      const { data } = await axiosInstance.get("/users/check", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      setUser(data);
       console.log(data);
-      
-    }catch(error){
+    } catch (error) {
       console.log(error.response);
-      navigate("/login")
-      
+      navigate("/login");
     }
-    
   }
 
-  useEffect(()=>{
-    checkUser()
-  }, [])
+  useEffect(() => {
+    checkUser();
+  }, []);
   return (
-    <AppStates.Provider value={{user, setUser}}>
+    <AppStates.Provider value={{ user, setUser }}>
       <Header />
-        <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path='/howitworks' element={<Howitworks />} />
-            <Route path="/home" element={<UserLogedIn />} />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/howitworks" element={<Howitworks />} />
+        <Route path="/home" element={<UserLogedIn />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/AskQuestion" element={< AskQuestion/>} />
-        </Routes>
-        <Footer />
+        <Route path="/AskQuestion" element={<AskQuestion />} />
+        <Route path="/answeres" element={<Answere />} />
+      </Routes>
+      <Footer />
     </AppStates.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
