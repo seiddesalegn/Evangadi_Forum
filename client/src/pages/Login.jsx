71c198b33f128320d7fb414d71a26be291721a40
Joseph Teka <179788482+Josy216@users.noreply.github.com>
2025-06-13 08:02:08 +0300

    Update Login.jsx
    just added password show/hide togle

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,7 @@
-import React, { useRef } from "react";
+
+import React, { useState } from "react";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import axiosInstance from "../Axios";
 import { Link, useNavigate } from "react-router-dom";
 import style from "./login.module.css";
@@ -7,19 +10,20 @@ import { useState } from "react";
 
 function Login() {
   const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const email = useRef(null);
   const password = useRef(null);
   async function handleSubmit(event) {
     event.preventDefault();
-    const emailValue = email.current.value;
-    const passwordValue = password.current.value;
 
     try {
       const { data } = await axiosInstance.post("/user/login", {
-        email: emailValue,
-        password: passwordValue,
+        email,
+        password,
       });
 
       localStorage.setItem("token", data.token);
@@ -32,11 +36,11 @@ function Login() {
   return (
     <section className={style.wrapper}>
       <div className={style.login}>
-        {error && <span style={{ color: "red" }}>something went wrong</span>}
+        {error && <span style={{ color: "red" }}>Something went wrong</span>}
         <h2>Login to your Account</h2>
         <div>
           <span>
-            Don't have account?
+            Don't have an account?{" "}
             <Link to={"/register"}>Create Account</Link>
           </span>
         </div>
@@ -45,10 +49,11 @@ function Login() {
             <label htmlFor="email">Email</label>
             <input
               type="email"
-              ref={email}
               id="email"
               name="email"
               placeholder="Enter your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
@@ -56,15 +61,22 @@ function Login() {
           <div>
             <label htmlFor="password">Password</label>
             <input
-              type="password"
-              ref={password}
+              type={showPassword ? "text" : "password"}
               id="password"
               name="password"
               placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               required
+              style={{ paddingRight: "10px" }}
             />
+            <span
+              onClick={() => setShowPassword((prev) => !prev)}>
+              {showPassword ? (<VisibilityIcon />) : (<VisibilityOffIcon />)}
+            </span>
           </div>
-          <button type="submit">login</button>
+          <br />
+          <button type="submit">Login</button>
         </form>
       </div>
       <About />
