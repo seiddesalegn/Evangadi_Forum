059bcee6e339a4d8a4d562ae22f6371fc492b8d1
Bersie1 <bersabe.tek@gmail.com>
2025-06-16 20:10:43 -0400

    Update ASKQ

--- client/src/Components/AskQuestion/AskQuestion.jsx
+++ client/src/Components/AskQuestion/AskQuestion.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./AskQuestion.css";
-import axiosInstance from "../../Axios";
+import axiosInstance from "../../Axios"; //pre-configured instance to make api request
 import ArrowCircleRightRoundedIcon from '@mui/icons-material/ArrowCircleRightRounded';
 
 function AskQuestion() {
   const [title, setTitle] = useState("");
-  const [detail, setDetail] = useState("");
-  const navigate = useNavigate();
+  const [detail, setDetail] = useState(""); //stores the question detail & disc.
+  const navigate = useNavigate(); //hook for navigating after form submission
 
+  //form submission - prevent default page reload
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token"); //gets auth token from local storage
 
-    try {
+    try {//sends post request to question endpoint w/form data
       await axiosInstance.post(
         "question",
         {
@@ -23,19 +24,20 @@ function AskQuestion() {
         },
         {
           headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${token}`,//token auth header
           },
         }
       );
-
+      //clears input field after successful submission
       setTitle("");
       setDetail("");
-      navigate("/");
-    } catch (err) {
+      navigate("/");//navigate bk to home
+    } catch (err) { //logs error if api request fails 
       console.error("Error posting question:", err);
     }
   };
 
+    //return to render the form UI
   return (
     <div className="ask-container">
       <div className="ask-wrapper">
@@ -74,15 +76,13 @@ function AskQuestion() {
             type="text"
             placeholder="Question title"
             name="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            onChange={(e) => setTitle(e.target.value)}//gets input value from title 
           />
           <textarea
             className="ask-textarea"
             placeholder="Question detail ..."
             name="detail"
-            value={detail}
-            onChange={(e) => setDetail(e.target.value)}
+            onChange={(e) => setDetail(e.target.value)}//gets input value from description 
           ></textarea>
 
           <button type="submit" className="ask-button">
