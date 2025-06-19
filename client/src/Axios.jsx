5b65c63d10acc6620b7b7e3c218d3b54eaf65123
Dawit <tesfayedawit22090582@gmail.com>
2025-06-19 10:04:02 +0300

    axios update

--- client/src/Axios.jsx
+++ client/src/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://api.evanforum.com/",
+  baseURL: "https://api.evanforum.com/api",
 });
 
 export default axiosInstance;
