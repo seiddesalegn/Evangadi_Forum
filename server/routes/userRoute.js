f08dede880bf3418d1cf837a64c97bae442be4ea
Joseph Teka <179788482+Josy216@users.noreply.github.com>
2025-06-27 10:57:50 +0300

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 
 // user controllers import
-const { register, login, checkUser } = require("../controller/userController");
+const { register, login, checkUser, reset } = require("../controller/userController");
 
 // register route
 router.post("/register", register);
@@ -12,7 +12,12 @@ router.post("/register", register);
 // login route
 router.post("/login", login);
 
+//reset password
+router.delete("/delete", reset);
+
+
 // check user
 router.get("/check", authMiddleware, checkUser);
+
 
 module.exports = router;
